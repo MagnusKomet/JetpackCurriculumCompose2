41036906aac91ef8ce617dd8ee56484b998ae97b
MagnusKomet <ehosta.dam@institutcampalans.net>
2025-03-27 16:22:26 +0100

    hecho

--- app/src/main/java/com/example/jetpackcurriculumcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcurriculumcompose/MainActivity.kt
@@ -1,30 +1,34 @@
 package com.example.jetpackcurriculumcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.*
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityOptionsCompat.BackgroundActivityStartMode
 import com.example.jetpackcurriculumcompose.ui.theme.JetpackCurriculumComposeTheme
 import org.w3c.dom.Text
 
@@ -32,26 +36,105 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            ViewContainer()
         }
     }
 }
 
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun App() {
+fun ViewContainer() {
+    Scaffold(
+        topBar = { Toolbar()},
+        content = {padding -> // Captura el padding
+            Content(modifier = Modifier.padding(padding))},
+        floatingActionButton = {FAB()},
+        floatingActionButtonPosition = FabPosition.End
+    )
+}
+
+@Composable
+fun FAB(){
+    val context = LocalContext.current
+    FloatingActionButton(onClick = {
+        Toast.makeText(context,"Funcionalitat en construcció", Toast.LENGTH_SHORT).show()
+    }){
+        Text(text = "o")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun Toolbar(){
+    TopAppBar(
+        title = { Text( text = "Ernest")}
+    )
+}
+
+@Composable
+fun Content(modifier: Modifier = Modifier) {
+    var counterC by rememberSaveable { mutableIntStateOf(0) }
+    var counterK by rememberSaveable { mutableIntStateOf(0) }
+
     LazyColumn(
         modifier = Modifier
-        .fillMaxSize()
-        .background(Color.DarkGray)
+            .fillMaxSize()
+            .background(Color.DarkGray)
     ) {
         item {
+
             Image(
                 painter = painterResource(id = R.drawable.avatar),
                 contentDescription = "My Photo",
-                modifier = Modifier.fillMaxWidth().height(400.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .padding(top = 100.dp)
             )
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(top = 8.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.c_sharp),
+                    contentDescription = "C# logo",
+                    modifier = Modifier
+                        .size(70.dp)
+                        .clickable { counterC++ }
+                )
+
+                Text(
+                    text = counterC.toString(),
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    modifier = Modifier.padding(start = 4.dp, top = 45.dp)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = counterK.toString(),
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    modifier = Modifier.padding(end = 10.dp, top = 45.dp)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.kotlin),
+                    contentDescription = "Kotlin logo",
+                    modifier = Modifier
+                        .size(70.dp)
+                        .clickable { counterK++ }
+                )
+
+
+            }
+
+
+
             Text(
                 text = "Ernest",
                 fontSize = 32.sp,
@@ -61,21 +144,18 @@ fun App() {
             )
             Text(text = "Informació de contacte", color = Color.White, fontSize = 20.sp)
 
-            LazyRow(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
-            ){
-                item{
-
-                    Text(text = "Ernest ", color = Color.White)
-                    Text(text = "Hosta ", color = Color.White)
-                    Text(text = "Vidal ", color = Color.White)
-                }
-            }
-
+            Text(text = "Ernest Hosta Vidal", color = Color.White)
+            
             Text(text = "ehosta.dam@institutcampalans.net", color = Color.White)
 
             Text(text = "https://github.com/MagnusKomet", color = Color.White)
+
+
+
+
+
+            
+            
         }
     }
 }
